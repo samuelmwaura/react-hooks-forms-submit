62c07d54d73059a276b0f3c09ebba474d7f17301
Samuel Mwaura <samuelmwaura081@gmail.com>
2022-07-19 13:40:18 +0300

    Completed the code along.

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,8 +1,10 @@
 import React, { useState } from "react";
 
 function Form(props) {
-  const [firstName, setFirstName] = useState("Sylvia");
+  const [firstName, setFirstName] = useState("");
   const [lastName, setLastName] = useState("Woods");
+  const [formSubmittedData,setformSubmittedData]= useState([])
+  const [errorMessage,setErrorMessage]= useState('');
 
   function handleFirstNameChange(event) {
     setFirstName(event.target.value);
@@ -12,12 +14,34 @@ function Form(props) {
     setLastName(event.target.value);
   }
 
+  function handleSubmit(event){
+  event.preventDefault();
+  if(firstName.length>0){
+  const formData = {firstName,lastName}
+  const dataArray =[...formSubmittedData,formData]
+  setformSubmittedData(()=>dataArray); //the inner callback function returns the formSubmittedData since return is implied
+  setFirstName('');
+  setLastName('');
+}else{
+  setErrorMessage('The First Name is required!')
+}
+
+  }
   return (
-    <form>
+    <>
+    <form onSubmit={handleSubmit}>
       <input type="text" onChange={handleFirstNameChange} value={firstName} />
       <input type="text" onChange={handleLastNameChange} value={lastName} />
       <button type="submit">Submit</button>
     </form>
+    <h3>Submissions</h3>
+    {errorMessage.length>0?
+    <p style={{color:'red'}}>{errorMessage}</p>:null
+  }
+    {formSubmittedData.map((data,index)=>{
+      return <div key={index}>{data.firstName}  {data.lastName}</div>
+    })}
+    </>
   );
 }
 
